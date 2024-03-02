d8b65c00357300ebb48140980a0cb79bd22ccb65
Mertcan <94529144+Mert7can18@users.noreply.github.com>
2024-03-03 00:46:38 +0200

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-// script.js dosyası
-document.addEventListener("DOMContentLoaded", function() {
-    var header = document.querySelector("header");
-
-    // Header'a mouse ile hover edildiğinde arka plan rengini değiştir
-    header.addEventListener("mouseenter", function() {
-        header.classList.add("header-animation");
-        header.style.backgroundColor = "#999"; // Yeni arka plan rengi
-    });
-
-    // Header üzerinden mouse çekildiğinde arka plan rengini eski haline getir
-    header.addEventListener("mouseleave", function() {
-        header.classList.remove("header-animation");
-        header.style.backgroundColor = "#c5c4c4"; // Eski arka plan rengi
-    });
+// Footer'ın sayfa en altına kaydırılması için bir event listener ekleyin
+window.addEventListener('scroll', function() {
+    var footer = document.querySelector('footer');
+    // Sayfa scroll edildiğinde, footer'ın görünürlüğünü kontrol edin
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        footer.style.display = 'block'; // Footer'ı görünür yapın
+    } else {
+        footer.style.display = 'none'; // Footer'ı gizleyin
+    }
 });
