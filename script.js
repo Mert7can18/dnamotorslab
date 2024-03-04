050425e3f3d8880ce75a847b6c722d4c7473684e
Mertcan <94529144+Mert7can18@users.noreply.github.com>
2024-03-04 09:34:52 +0200

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// Footer'ın sayfa en altına kaydırılması için bir event listener ekleyin
 window.addEventListener('scroll', function() {
     var footer = document.querySelector('footer');
-    // Sayfa scroll edildiğinde, footer'ın görünürlüğünü kontrol edin
+    var main = document.querySelector('main');
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-        footer.style.display = 'block'; // Footer'ı görünür yapın
+        footer.style.display = 'block';
+        main.style.marginBottom = footer.offsetHeight + 'px';
     } else {
-        footer.style.display = 'none'; // Footer'ı gizleyin
+        footer.style.display = 'none';
+        main.style.marginBottom = '0';
     }
 });
